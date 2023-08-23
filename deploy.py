28215320807a80642bac6c71a098bac49a2d1279
asdfry <asdfry@naver.com>
2023-08-23 16:04:30 +0900

    Update: update for ADD

--- deploy.py
+++ deploy.py
@@ -3,7 +3,7 @@ import argparse
 from kubernetes import client, config
 
 
-def create_master(node, port, gpu, slot):
+def create_master(node, port, gpu, slot, model_dir_path):
     # global host_addr
     # ip = f"{network_addr}.{host_addr}"
     pod_manifest = {
@@ -21,7 +21,7 @@ def create_master(node, port, gpu, slot):
                 {
                     "name": "app",
                     "image": f"{args.image_name}-{args.version}",
-                    "env": [{"name": "NCCL_SOCKET_IFNAME", "value": f"ib{port-1040}"}],
+                    # "env": [{"name": "NCCL_SOCKET_IFNAME", "value": f"ib{port-1040}"}],
                     # "imagePullPolicy": "Always",
                     "volumeMounts": [{"name": "pretrained-models", "mountPath": "/root/pretrained-models"}],
                     "command": [
@@ -38,15 +38,16 @@ def create_master(node, port, gpu, slot):
                     },
                 },
             ],
-            "volumes": [{"name": "pretrained-models", "persistentVolumeClaim": {"claimName": "jsh-pvc"}}],
+            # "volumes": [{"name": "pretrained-models", "persistentVolumeClaim": {"claimName": "jsh-pvc"}}],
+            "volumes": [{"name": "pretrained-models", "hostPath": {"path": f"{model_dir_path}", "type": "Directory"}}],
         },
     }
     v1.create_namespaced_pod(namespace, pod_manifest)
-    print(f"POD (name: {pod_manifest['metadata']['name']}, node: {node}, ip: {ip}, port: {port})")
-    host_addr += 1
+    print(f"POD (name: {pod_manifest['metadata']['name']}, node: {node}, port: {port})")
+    # host_addr += 1
 
 
-def create_worker(node, port, gpu, slot):
+def create_worker(node, port, gpu, slot, model_dir_path):
     global worker_num
     # global worker_num, host_addr
     # ip = f"{network_addr}.{host_addr}"
@@ -65,7 +66,7 @@ def create_worker(node, port, gpu, slot):
                 {
                     "name": "app",
                     "image": f"{args.image_name}-{args.version}",
-                    "env": [{"name": "NCCL_SOCKET_IFNAME", "value": f"ib{port-1040}"}],
+                    # "env": [{"name": "NCCL_SOCKET_IFNAME", "value": f"ib{port-1040}"}],
                     # "imagePullPolicy": "Always",
                     "volumeMounts": [{"name": "pretrained-models", "mountPath": "/root/pretrained-models"}],
                     "command": [
@@ -82,18 +83,20 @@ def create_worker(node, port, gpu, slot):
                     },
                 },
             ],
-            "volumes": [{"name": "pretrained-models", "persistentVolumeClaim": {"claimName": "jsh-pvc"}}],
+            # "volumes": [{"name": "pretrained-models", "persistentVolumeClaim": {"claimName": "jsh-pvc"}}],
+            "volumes": [{"name": "pretrained-models", "hostPath": {"path": f"{model_dir_path}", "type": "Directory"}}],
         },
     }
     v1.create_namespaced_pod(namespace, pod_manifest)
-    print(f"POD (name: {pod_manifest['metadata']['name']}, node: {node}, ip: {ip}, port: {port})")
-    host_addr += 1
+    print(f"POD (name: {pod_manifest['metadata']['name']}, node: {node}, port: {port})")
+    # host_addr += 1
     worker_num += 1
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--image_name", type=str, required=True)
+    parser.add_argument("-m", "--model_dir_path", type=str, required=True)
     parser.add_argument("-s", "--slot_size", type=int, required=True)
     parser.add_argument("-t", "--total_node", type=int, required=True)
     parser.add_argument("-v", "--version", type=str, required=True)
@@ -113,7 +116,7 @@ if __name__ == "__main__":
     # host_addr = int(addr.split(".")[-1])
 
     node = f"gnode{args.master_node_num}"
-    create_master(node, 1041, args.gpu_master, args.slot_size)
+    create_master(node, 1041, args.gpu_master, args.slot_size, args.model_dir_path)
     # for i in range(1, args.slot_size):
     #     create_worker(node, 1041 + i, args.gpu_master)
 
@@ -121,4 +124,4 @@ if __name__ == "__main__":
         node_num = str(args.master_node_num + i).zfill(3)
         node = f"gnode{node_num}"
         # for i in range(0, args.slot_size):
-        create_worker(node, 1041, args.gpu_worker, args.slot_size)
+        create_worker(node, 1041, args.gpu_worker, args.slot_size, args.model_dir_path)
