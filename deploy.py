dfb9a01c3c888b31979de09f2c68ca7ca3fbeba6
asdfry <asdfry@naver.com>
2023-08-23 16:16:41 +0900

    Fix: fix minor bug

--- deploy.py
+++ deploy.py
@@ -96,12 +96,12 @@ def create_worker(node, port, gpu, slot, model_dir_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--image_name", type=str, required=True)
-    parser.add_argument("-m", "--model_dir_path", type=str, required=True)
     parser.add_argument("-s", "--slot_size", type=int, required=True)
     parser.add_argument("-t", "--total_node", type=int, required=True)
     parser.add_argument("-v", "--version", type=str, required=True)
     # parser.add_argument("-ma", "--master_addr", type=str, required=True)
     parser.add_argument("-mn", "--master_node_num", type=int, required=True)
+    parser.add_argument("-mp", "--model_dir_path", type=str, required=True)
     parser.add_argument("-gm", "--gpu_master", type=str, required=True)
     parser.add_argument("-gw", "--gpu_worker", type=str, required=True)
     args = parser.parse_args()
@@ -115,7 +115,8 @@ if __name__ == "__main__":
     # network_addr = addr[: addr.rfind(".")]
     # host_addr = int(addr.split(".")[-1])
 
-    node = f"gnode{args.master_node_num}"
+    node_num = str(args.master_node_num).zfill(3)
+    node = f"gnode{node_num}"
     create_master(node, 1041, args.gpu_master, args.slot_size, args.model_dir_path)
     # for i in range(1, args.slot_size):
     #     create_worker(node, 1041 + i, args.gpu_master)
