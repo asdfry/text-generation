f75b6b4a77702e57a35631d029e6a90b38df1ffe
asdfry <asdfry@naver.com>
2023-09-14 01:08:21 +0000

    Fix: fix typo

--- k8s/run.sh
+++ k8s/run.sh
@@ -2,6 +2,6 @@
 
 docker rm -f deploy-pod
 docker build -t asdfry/python:3.10.6-slim-kube .
-yes | docker prune iamge
+yes | docker iamge prune
 sh ~/workspaces/gdrive/add-deploy/deploy_cont.sh
 docker exec -it deploy-pod /bin/bash
