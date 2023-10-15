a290b5f859b3f96fd82528550fc83b2ce5f00206
asdfry <asdfry@naver.com>
2023-10-16 00:00:26 +0900

    Update: update torch profiler

--- demo/train.py
+++ demo/train.py
@@ -8,7 +8,7 @@ from datasets import load_dataset
 from accelerate import Accelerator
 from torch.optim import SGD, AdamW
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from torch.profiler import profile
+from torch.profiler import profile, schedule, tensorboard_trace_handler, ProfilerActivity
 from torch.utils.data import DataLoader
 from accelerate.logging import get_logger
 
@@ -44,17 +44,12 @@ if accelerator.process_index == 0:
     logger = get_logger(__name__)
 
 
-# Prefix
+# Create datase
 dataset_name = "JulesBelveze/tldr_news"
-if accelerator.process_index == 0:
-    logger.info(f"Model: {args.model_name}")
-
-
-# Create dataset
 datasets = load_dataset(dataset_name)
 
 
-# Load tokenizer
+# Load tokenize
 tokenizer = AutoTokenizer.from_pretrained(args.model_name)
 tokenizer.pad_token = tokenizer.eos_token
 
@@ -67,7 +62,7 @@ def tokenize_function(examples):
 
 # Tokenize
 # before tokenize: ['headline', 'content', 'category']
-# after tokenize: ['input_ids', 'attention_mask', 'labels']
+# after tokenize: ['input_ids', 'attention_mask', 'labels'
 tokenized_datasets = datasets.map(
     tokenize_function,
     batched=True,
@@ -77,7 +72,7 @@ tokenized_datasets = datasets.map(
 tokenized_datasets.set_format("torch")
 
 
-# Create dataloader
+# Create dataloade
 train_dataset = (
     tokenized_datasets["train"]
     .shuffle(seed=77)
@@ -89,22 +84,28 @@ valid_dataset = (
     .select(range(int(tokenized_datasets["test"].num_rows * args.dataset_size)))
 )
 
+if accelerator.process_index == 0:
+    logger.info(f"Train dataset size: {len(train_dataset)}")
+    logger.info(f"Valid dataset size: {len(valid_dataset)}")
+
 train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size)
 valid_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size)
 
 
-# Load model
+# Load mode
+if accelerator.process_index == 0:
+    logger.info(f"Model: {args.model_name}")
 model = AutoModelForCausalLM.from_pretrained(args.model_name)
 
 
-# Set optimizer
+# Set optimize
 if args.optimizer == "sgd":
     optimizer = SGD(model.parameters(), lr=1e-5)
 elif args.optimizer == "adamw":
     optimizer = AdamW(model.parameters(), lr=1e-5)
 
 
-# Ready for training with accelerate
+# Ready for training with accelerat
 model, optimizer, train_dataloader, valid_dataloader = accelerator.prepare(
     model,
     optimizer,
@@ -113,55 +114,16 @@ model, optimizer, train_dataloader, valid_dataloader = accelerator.prepare(
 )
 
 
-# Start training
+# Start training with profiler
 if accelerator.process_index == 0:
-    start_time = time.time()
-    logger.info(f"Start training")
+    with profile(
+        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+        schedule=schedule(wait=1, warmup=1, active=2, repeat=1),
+        on_trace_ready=tensorboard_trace_handler(dirpath),
+    ) as prof:
+        start_time = time.time()
+        logger.info(f"Start training")
 
-    with profile(with_stack=True) as prof:
-        # Iterate data loader
-        for epoch in range(args.epoch):
-            # >>> Train >>>
-            model.train()
-            loss_per_epoch = 0
-
-            for step, batch in enumerate(train_dataloader):
-                batch = {k: v for k, v in batch.items()}
-                outputs = model(**batch)
-                loss = outputs.loss
-                loss_per_epoch += loss
-                accelerator.backward(loss)
-                optimizer.step()
-                optimizer.zero_grad()
-
-                logger.info(
-                    f"[epoch {epoch+1}] train step: {step + 1}/{len(train_dataloader)}, loss: {loss_per_epoch / (step + 1)}"
-                )
-            # <<< Train <<<
-
-            # >>> Valid >>>
-            model.eval()
-            loss_per_epoch = 0
-
-            for step, batch in enumerate(valid_dataloader):
-                batch = {k: v for k, v in batch.items()}
-                with torch.no_grad():
-                    outputs = model(**batch)
-                loss_per_epoch += outputs.loss
-
-                logger.info(
-                    f"[epoch {epoch+1}] valid step: {step + 1}/{len(valid_dataloader)}, loss: {loss_per_epoch / (step + 1)}"
-                )
-            # <<< Valid <<<
-
-            logger.info(f"[epoch {epoch+1}] elapsed time: {time.time() - start_time} sec")
-
-    prof.export_chrome_trace(f"{dirpath}/trace.json")
-    logger.info(f"Result of profile\n{prof.key_averages(group_by_stack_n=1).table(sort_by='self_cpu_time_total', row_limit=-1, max_name_column_width=110)}")
-
-else:
-    # Iterate data loader
-    for epoch in range(args.epoch):
         # >>> Train >>>
         model.train()
         loss_per_epoch = 0
@@ -174,15 +136,55 @@ else:
             accelerator.backward(loss)
             optimizer.step()
             optimizer.zero_grad()
+
+            if accelerator.process_index == 0:
+                logger.info(
+                    f"[epoch 1] train step: {step + 1}/{len(train_dataloader)}, loss: {loss_per_epoch / (step + 1)}"
+                )
+            prof.step()
         # <<< Train <<<
 
         # >>> Valid >>>
-        model.eval()
-        loss_per_epoch = 0
+        # model.eval()
+        # loss_per_epoch = 0
 
-        for step, batch in enumerate(valid_dataloader):
-            batch = {k: v for k, v in batch.items()}
-            with torch.no_grad():
-                outputs = model(**batch)
-            loss_per_epoch += outputs.loss
+        # for step, batch in enumerate(valid_dataloader):
+        #     batch = {k: v for k, v in batch.items()}
+        #     with torch.no_grad():
+        #         outputs = model(**batch)
+        #     loss_per_epoch += outputs.loss
+
+        #     if accelerator.process_index == 0:
+        #         logger.info(
+        #             f"[epoch 1] valid step: {step + 1}/{len(valid_dataloader)}, loss: {loss_per_epoch / (step + 1)}"
+        #         )
+        #     prof.step()
         # <<< Valid <<<
+
+        logger.info(f"[epoch 1] elapsed time: {time.time() - start_time} sec")
+
+else:
+    # >>> Train >>>
+    model.train()
+    loss_per_epoch = 0
+
+    for step, batch in enumerate(train_dataloader):
+        batch = {k: v for k, v in batch.items()}
+        outputs = model(**batch)
+        loss = outputs.loss
+        loss_per_epoch += loss
+        accelerator.backward(loss)
+        optimizer.step()
+        optimizer.zero_grad()
+    # <<< Train <<<
+
+    # >>> Valid >>>
+    # model.eval()
+    # loss_per_epoch = 0
+
+    # for step, batch in enumerate(valid_dataloader):
+    #     batch = {k: v for k, v in batch.items()}
+    #     with torch.no_grad():
+    #         outputs = model(**batch)
+    #     loss_per_epoch += outputs.loss
+    # <<< Valid <<<
