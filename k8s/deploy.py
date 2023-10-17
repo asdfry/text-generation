6c84939c5f0508e0acddcce6a8741ec23194a7fc
asdfry <asdfry@naver.com>
2023-10-17 22:01:14 +0900

    Update: update for ADD

--- k8s/deploy.py
+++ k8s/deploy.py
@@ -3,22 +3,22 @@ import argparse
 from kubernetes import client, config
 
 
-def create_master(node: str, port: int, gpu: str, slot: int, log_dir_path: str, model_dir_path: str):
+def create_pod(name: str, hostname: str, gpu: str):
     pod_manifest = {
         "apiVersion": "v1",
         "kind": "Pod",
         "metadata": {
-            "name": "master",
+            "name": name,
             "namespace": namespace,
             "labels": {"ten1010.io/creator-name": "jsh"},
         },
         "spec": {
             "hostNetwork": True,
-            "nodeSelector": {"kubernetes.io/hostname": node},
+            "nodeSelector": {"kubernetes.io/hostname": hostname},
             "containers": [
                 {
                     "name": "app",
-                    "image": f"{args.image_name}:{args.version}",
+                    "image": f"{args.image_name}",
                     # "imagePullPolicy": "Always",
                     "volumeMounts": [
                         {"name": "logs", "mountPath": "/root/logs"},
@@ -28,14 +28,10 @@ def create_master(node: str, port: int, gpu: str, slot: int, log_dir_path: str, 
                     "command": [
                         "/bin/bash",
                         "-c",
-                        f"/usr/sbin/sshd -p {port} && sleep infinity",
+                        f"/usr/sbin/sshd -p 1041 && sleep infinity",
                     ],
-                    "ports": [{"containerPort": port, "hostPort": port, "protocol": "TCP"}],
-                    "resources": {
-                        "limits": {
-                            gpu: str(slot),
-                        }
-                    },
+                    "ports": [{"containerPort": 1041, "hostPort": 1041, "protocol": "TCP"}],
+                    "resources": {"limits": {gpu: str(args.slot_size)}},
                     "securityContext": {"privileged": True},
                 },
             ],
@@ -45,80 +41,24 @@ def create_master(node: str, port: int, gpu: str, slot: int, log_dir_path: str, 
             #     {"name": "shmdir", "emptyDir": {"medium": "Memory", "sizeLimit": "256M"}},
             # ],
             "volumes": [
-                {"name": "logs", "hostPath": {"path": f"{log_dir_path}", "type": "Directory"}},
-                {"name": "pretrained-models", "hostPath": {"path": f"{model_dir_path}", "type": "Directory"}},
-                {"name": "shmdir", "emptyDir": {"medium": "Memory", "sizeLimit": "256M"}},
-            ],
-        },
-    }
-    v1.create_namespaced_pod(namespace, pod_manifest)
-    print(f"POD (name: {pod_manifest['metadata']['name']}, node: {node}, port: {port})")
-
-
-def create_worker(node: str, port: int, gpu: str, slot: int, log_dir_path: str, model_dir_path: str):
-    global worker_num
-    pod_manifest = {
-        "apiVersion": "v1",
-        "kind": "Pod",
-        "metadata": {
-            "name": f"worker-{worker_num}",
-            "namespace": namespace,
-            "labels": {"ten1010.io/creator-name": "jsh"},
-        },
-        "spec": {
-            "hostNetwork": True,
-            "nodeSelector": {"kubernetes.io/hostname": node},
-            "containers": [
+                {"name": "logs", "hostPath": {"path": f"{args.log_dir_path}", "type": "Directory"}},
                 {
-                    "name": "app",
-                    "image": f"{args.image_name}:{args.version}",
-                    # "imagePullPolicy": "Always",
-                    "volumeMounts": [
-                        {"name": "logs", "mountPath": "/root/logs"},
-                        {"name": "pretrained-models", "mountPath": "/root/pretrained-models"},
-                        {"name": "shmdir", "mountPath": "/dev/shm"},
-                    ],
-                    "command": [
-                        "/bin/bash",
-                        "-c",
-                        f"/usr/sbin/sshd -p {port} && sleep infinity",
-                    ],
-                    "ports": [{"containerPort": port, "hostPort": port, "protocol": "TCP"}],
-                    "resources": {
-                        "limits": {
-                            gpu: str(slot),
-                        }
-                    },
-                    "securityContext": {"privileged": True},
+                    "name": "pretrained-models",
+                    "hostPath": {"path": f"{args.model_dir_path}", "type": "Directory"},
                 },
-            ],
-            # "volumes": [
-            #     {"name": "logs", "persistentVolumeClaim": {"claimName": "jsh-logs-pvc"}},
-            #     {"name": "pretrained-models", "persistentVolumeClaim": {"claimName": "jsh-pretrained-models-pvc"}},
-            #     {"name": "shmdir", "emptyDir": {"medium": "Memory", "sizeLimit": "256M"}},
-            # ],
-            "volumes": [
-                {"name": "logs", "hostPath": {"path": f"{log_dir_path}", "type": "Directory"}},
-                {"name": "pretrained-models", "hostPath": {"path": f"{model_dir_path}", "type": "Directory"}},
                 {"name": "shmdir", "emptyDir": {"medium": "Memory", "sizeLimit": "256M"}},
             ],
         },
     }
     v1.create_namespaced_pod(namespace, pod_manifest)
-    print(f"POD (name: {pod_manifest['metadata']['name']}, node: {node}, port: {port})")
-    worker_num += 1
+    print(f"POD (name: {name}, node: {hostname}, gpu: {gpu} x {args.slot_size})")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--image_name", type=str, required=True)
-    parser.add_argument("-n", "--node_prefix", type=str, required=True)
     parser.add_argument("-s", "--slot_size", type=int, required=True)
-    parser.add_argument("-t", "--total_node", type=int, required=True)
-    parser.add_argument("-v", "--version", type=str, required=True)
-    parser.add_argument("-z", "--zero_fill", type=int, required=True)
     parser.add_argument("-lp", "--log_dir_path", type=str, required=True)
-    parser.add_argument("-mn", "--master_node_num", type=int, required=True)
     parser.add_argument("-mp", "--model_dir_path", type=str, required=True)
     parser.add_argument("-gm", "--gpu_master", type=str, required=True)
     parser.add_argument("-gw", "--gpu_worker", type=str, required=True)
@@ -126,15 +66,13 @@ if __name__ == "__main__":
 
     config.load_kube_config()
     v1 = client.CoreV1Api()
-
     namespace = "common"
-    worker_num = 1
 
-    node_num = str(args.master_node_num).zfill(args.zero_fill)
-    node = f"{args.node_prefix}{node_num}"
-    create_master(node, 1041, args.gpu_master, args.slot_size, args.log_dir_path, args.model_dir_path)
+    with open("pods", "r") as f:
+        lines = f.readlines()
 
-    for i in range(1, args.total_node):
-        node_num = str(args.master_node_num + i).zfill(args.zero_fill)
-        node = f"{args.node_prefix}{node_num}"
-        create_worker(node, 1041, args.gpu_worker, args.slot_size, args.log_dir_path, args.model_dir_path)
+    for idx, hostname in enumerate(lines):
+        if idx == 0:
+            create_pod("master", hostname.strip(), args.gpu_master)
+        else:
+            create_pod(f"worker-{idx}", hostname.strip(), args.gpu_worker)
